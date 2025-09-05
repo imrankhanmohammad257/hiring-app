9a40ec9d74e6ddf903e4a7384243a201be2543d1
MOHAMMAD IMRAN KHAN <imrankhanmohammad257@gmail.com>
2025-09-05 19:57:50 +0530

    Update slack.groovy

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -1,9 +1,12 @@
-def call() {
+def call(String status = "SUCCESS") {
+    def color = (status == "SUCCESS") ? "good" : "danger"
+    def emoji = (status == "SUCCESS") ? "✅" : "❌"
+
     slackSend(
         channel: '#jenkins-integration',
-        color: 'good',
+        color: color,
         message: """🚀 Jenkins pipeline update:
-✅ *Build stage* and ✅ *Slack notification stage* have been moved to the *Jenkins Shared Library*.
-Now the Jenkinsfile is clean and reusable! 🎉"""
+hiring-app Job finished with status: *${status}* ${emoji}
+(From Shared Library)"""
     )
 }
