b07ad4984e282178c12a2c89b12c41044fa98f51
MOHAMMAD IMRAN KHAN <imrankhanmohammad257@gmail.com>
2025-09-10 18:08:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,6 @@
 resource "local_file" "my_pet" {
-  filename = var.filename
   content  = var.content
-}
-
-resource "random_pet" "petname" {
-  prefix    = var.prefix
-  separator = "."
-  length    = 1
+  filename = "/home/ec2-user/hiring_app/${var.filename}"  # absolute path
+  file_permission      = "0777"
+  directory_permission = "0777"
 }
