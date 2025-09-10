a808a4e711a9d0eb8793485f92cc0c46d4e3a2ec
MOHAMMAD IMRAN KHAN <imrankhanmohammad257@gmail.com>
2025-09-10 18:10:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "local_file" "my_pet" {
   content  = var.content
-  filename = "/home/ec2-user/hiring_app/${var.filename}"  # absolute path
+  filename = "/var/lib/jenkins/workspace/terraform-maven/${var.filename}"
   file_permission      = "0777"
   directory_permission = "0777"
 }
